6e64d86e81ed0f7044c8d6a487d6543902cb189e
Yana Pavlova <yn-pvlv@yandex.ru>
2024-12-18 23:27:33 +0300

    chore: change Netlify deploy

--- next.config.js
+++ next.config.js
@@ -1,5 +1,27 @@
 module.exports = {
 	reactStrictMode: true,
+	trailingSlash: true,
+	async rewrites() {
+		return [
+			{
+				source: '/tasks/:id',
+				destination: '/tasks/[id]',
+			},
+		];
+	},
+	async exportPathMap() {
+		const paths = {};
+		paths['/'] = { page: '/' };
+		
+		for (let i = 1; i <= 200; i++) {
+			paths[`/tasks/${i}`] = {
+				page: '/tasks/[id]',
+				query: { id: i.toString() },
+			};
+		}
+		
+		return paths;
+	},
 	webpack: (config) => {
 		config.module.rules.push({
 			test: /\.svg$/,
